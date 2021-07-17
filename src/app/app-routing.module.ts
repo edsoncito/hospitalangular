b00ab0f9076d5a6927c27de43f33821cf91fbbfb
Edson Gonzalo Mamani Veizaga <edson.nur@gmail.com>
2021-07-17 13:30:14 -0400

    implementation of bootrap and creation of template
    s

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PacientesComponent } from './componentes/pacientes/pacientes.component';
+import { DoctoresComponent } from './componentes/doctores/doctores.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "pacientes", component: PacientesComponent },
+  { path: "doctores", component: DoctoresComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
+import { HolaMundoComponent } from './componentes/hola-mundo/hola-mundo.component';
+import { SidebarComponent } from './componentes/sidebar/sidebar.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { PacientesComponent } from './componentes/pacientes/pacientes.component';
+import { DoctoresComponent } from './componentes/doctores/doctores.component';
+import { EspecialidadesComponent } from './componentes/especialidades/especialidades.component';
+import { HospitalesComponent } from './componentes/hospitales/hospitales.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HolaMundoComponent
+    HolaMundoComponent,
+    SidebarComponent,
+    NavbarComponent,
+    PacientesComponent,
+    DoctoresComponent,
+    EspecialidadesComponent,
+    HospitalesComponent
   ],
   imports: [
     BrowserModule,
